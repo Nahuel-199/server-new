933f7a07f2190a445e2f1326c6e5f3882d39a2cc
Nahuel Cernadas <gabrielnahuel96@gmail.com>
2023-03-08 21:38:47 -0300

    serverout

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary");
+const connectDatabase = require("./src/config/database");
 const path = require("path");
 
 const errorMiddleware = require("./src/middleware/error");
@@ -28,6 +30,36 @@ app.use("/api", user);
 app.use("/api", order);
 app.use("/api", payment);
 
+app.use(errorMiddleware);
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
+// Connecting to database
+connectDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
 /* app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
@@ -35,6 +67,5 @@ app.get("*", (req, res) => {
 });
  */
 // Middleware for Errors
-app.use(errorMiddleware);
 
 module.exports = app;
